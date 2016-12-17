e34e470f153eb0d7d4d3773c7658d018c94f0f42
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-12-17 03:05:08 -0400

    commit

--- simulacionDigital/SimulacionCajeroBanco/clases/cajero.cs
+++ simulacionDigital/SimulacionCajeroBanco/clases/cajero.cs
@@ -12,6 +12,8 @@ namespace SimulacionCajeroBanco.clases
         public int clientesAtendidos { get; set; } //para saber cuales clientes fueron atendidos
         public int clientesCola { get; set; }//para saber todos los clientes que llegaron
         public double tiempoPromedioEnServcio { get; set; }//para saber el tiempo promedio servicio
+        public string operacion { get; set; }// tipo de caja que maneja el cajero
         private List<cliente> clientes { get; set; }
+        public int cantidad_cajeros_esta_operacion { get; set; }
     }
 }
